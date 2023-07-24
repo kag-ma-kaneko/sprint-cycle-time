a7278cb626850b76aedd0f38b3f0c3e7e170954c
ma-kaneko <ma-kaneko@kddi-agile.com>
2023-07-24 15:42:37 +0900

    静的なJSONは人が読みやすいように変更、プログラムで変換してApexChartsにわたすように変更

--- src/timeline.tsx
+++ src/timeline.tsx
@@ -1,20 +1,55 @@
 import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
+
+// JSONデータの形式に対応する型を定義
+type TaskData = {
+  name: string;
+  subtasks: {
+    name: string;
+    start: number;
+    end: number;
+  }[];
+}[];
+
+// ApexChartsのseriesの型定義
+type SeriesData = {
+  name: string;
+  data: {
+    x: string;
+    y: [number, number];
+  }[];
+}[];
 
 const Timeline = () => {
-  const [series, setSeries] = useState([]);
+  const [series, setSeries] = useState<SeriesData>([]);
 
+  // JSONデータを読み込んで、グラフに表示するデータを作成
+  // ApexChartsが欲しがるデータはサブタスクごとに分けられているので、
   useEffect(() => {
     fetch("data.json")
-      .then((response) => response.json())
-      .then((data) => setSeries(data));
+      .then((response) => response.json() as Promise<TaskData>)
+      .then((data) => {
+        const series = data.flatMap((task) =>
+          task.subtasks.map((subtask) => ({
+            name: subtask.name,
+            data: [
+              {
+                x: task.name,
+                y: [subtask.start, subtask.end] as [number, number],
+              },
+            ],
+          }))
+        );
+        setSeries(series);
+      });
   }, []);
 
   // グラフ表示形式の設定
   // 横向きで、横軸を時刻にしてTimelineを表示しています
-  const options = {
+  const options: ApexOptions = {
     chart: {
-      type: "rangeBar" as const,
+      type: "rangeBar",
     },
     plotOptions: {
       bar: {
@@ -22,7 +57,7 @@ const Timeline = () => {
       },
     },
     xaxis: {
-      type: "datetime" as const,
+      type: "datetime",
     },
     yaxis: {
       reversed: true,
