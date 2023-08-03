93d08547428d5880a4cbcb8ae3b310d883bf12d0
ma-kaneko <ma-kaneko@kddi-agile.com>
2023-08-03 14:43:10 +0900

    ファイル選択機能実装

--- src/timeline.tsx
+++ src/timeline.tsx
@@ -1,5 +1,5 @@
 import { Chart } from "react-google-charts";
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 
 // JSONデータの形式に対応する型を定義
 type SprintData = {
@@ -30,44 +30,58 @@ const Timeline = () => {
   const [data, setData] = useState<ChartData>([]);
   const [sprint, setSprint] = useState<SprintMetaData>();
 
-  useEffect(() => {
-    fetch("data.json")
-      .then((response) => response.json() as Promise<SprintData>)
-      .then((data) => {
-        setSprint(data.metaData);
-        const chartData: ChartData = data.backlogs.flatMap((backlog) =>
-          backlog.subtasks.map((subtask) => [
-            backlog.name,
-            subtask.name,
-            new Date(subtask.start),
-            new Date(subtask.end),
-          ])
-        );
-        setData(chartData);
-      });
-  }, []);
+  // ファイル選択時の処理
+  const handleFileChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const file = event.target.files?.[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const result = event.target?.result;
+          if (typeof result === "string") {
+            const data = JSON.parse(result) as SprintData;
+            setSprint(data.metaData);
+            const chartData: ChartData = data.backlogs.flatMap((backlog) =>
+              backlog.subtasks.map((subtask) => [
+                backlog.name,
+                subtask.name,
+                new Date(subtask.start),
+                new Date(subtask.end),
+              ])
+            );
+            setData(chartData);
+          }
+        };
+        reader.readAsText(file);
+      }
+    },
+    []
+  );
 
   return (
     <div className="App">
-      <Chart
-        width={"100%"}
-        height={"100vh"}
-        chartType="Timeline"
-        loader={<div>Loading Chart</div>}
-        data={[["Backlog Name", "Subtask Name", "Start", "End"], ...data]}
-        options={{
-          showRowNumber: true,
-          hAxis: {
-            format: "MM/dd HH:mm",
-            minValue: new Date(sprint?.beginDete ?? ""),
-            maxValue: new Date(sprint?.endDate ?? ""),
-          },
-          timeline: {
-            groupByRowLabel: true,
-          },
-        }}
-        rootProps={{ "data-testid": "1" }}
-      />
+      <input type="file" onChange={handleFileChange} className="file-input" />
+      {sprint && (
+        <Chart
+          width={"100%"}
+          height={"100vh"}
+          chartType="Timeline"
+          loader={<div>Loading Chart</div>}
+          data={[["Backlog Name", "Subtask Name", "Start", "End"], ...data]}
+          options={{
+            showRowNumber: true,
+            hAxis: {
+              format: "MM/dd HH:mm",
+              minValue: new Date(sprint?.beginDete ?? ""),
+              maxValue: new Date(sprint?.endDate ?? ""),
+            },
+            timeline: {
+              groupByRowLabel: true,
+            },
+          }}
+          rootProps={{ "data-testid": "1" }}
+        />
+      )}
     </div>
   );
 };
